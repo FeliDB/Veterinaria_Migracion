e0d56877df8ec0a68658573e6577175ab4ce52eb
Felipe <felipediazb16@gmail.com>
2025-07-01 16:21:41 -0300

    Eliminado script de autoinicio

--- arrancar-proyecto.bat
+++
@@ -1,9 +0,0 @@
-docker-compose down --volumes --remove-orphans 
-docker-compose build
-docker-compose up -d
-sleep 6
-docker-compose exec backend python manage.py makemigrations
-sleep 6
-docker-compose exec backend python manage.py migrate
-sleep 6
-python mongo_migrator/migrator.py
--- arrancar-proyecto.sh
+++
@@ -1,9 +0,0 @@
-docker-compose down --volumes --remove-orphans 
-docker-compose build
-docker-compose up -d
-wait 6
-docker-compose exec backend python manage.py makemigrations
-wait 6
-docker-compose exec backend python manage.py migrate
-wait 
-python mongo_migrator/migrator.py
